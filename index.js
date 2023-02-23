b010468805afa6560f5a65c7aa90809efc3bd95f
koyamal <tanakamaruko1121n@gmail.com>
2023-02-23 18:55:46 +0900

    Add dummy token

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const dotenv = require("dotenv");
 
-const result = dotenv.config();
-
-console.log(result);
-console.log(result.parsed);
-console.log(result.parsed.KEY1);
+const token = dotenv.config().parsed.token;
+console.log(token);
