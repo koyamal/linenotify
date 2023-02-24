30ea06ac2f7f18c5b9135f8f2001537afa8ac859
koyamal <tanakamaruko1121n@gmail.com>
2023-02-24 21:06:02 +0900

    Use axios for test

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const axios = require("axios");
 
 const token = dotenv.config().parsed.token;
 console.log(token);
+
+axios({
+    method: 'get',
+    url: 'http://bit.ly/2mTM3nY',
+    responseType: 'stream'
+  })
+    .then(function (response) {
+      console.log(response.data);
+    });
