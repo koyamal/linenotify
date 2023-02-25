61567789b102c94549c8d943f6fc3dccaf92ad90
koyamal <tanakamaruko1121n@gmail.com>
2023-02-25 09:16:34 +0900

    Error: await is only valid in async functions and the top level bodies of modules

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const dotenv = require("dotenv");
 const axios = require("axios");
 
-const token = dotenv.config().parsed.token;
+const token = dotenv.config().parsed.lineToken;
 console.log(token);
+const url = dotenv.config().parsed.url;
+console.log(url);
 
 axios({
-    method: 'get',
+    method: 'post',
+    ContentType: 'application/x-www-form-urlencoded',
     url: 'http://bit.ly/2mTM3nY',
     responseType: 'stream'
   })
     .then(function (response) {
       console.log(response.data);
     });
+
+const {data} = await axios.post(
+    url, 
+    {
+        message: 'This is Test',
+    }, 
+    {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + token
+        }
+    }
+)
+
+data();
