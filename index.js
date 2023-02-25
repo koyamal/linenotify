90974097264c79e229d7aec3933a9a4eaa378221
koyamal <tanakamaruko1121n@gmail.com>
2023-02-25 09:34:00 +0900

    Delete unnecessary code

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ const token = dotenv.config().parsed.lineToken;
 const url = dotenv.config().parsed.lineUrl;
 const message = dotenv.config().parsed.message;
 
-axios({
-    method: 'post',
-    ContentType: 'application/x-www-form-urlencoded',
-    url: 'http://bit.ly/2mTM3nY',
-    responseType: 'stream'
-  })
-    .then(function (response) {
-      //console.log(response.data);
-    });
-
 axios.post(
     url, 
     {
