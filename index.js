24da1cfba7b5fbe6c7066102edc7898759c77d11
koyamal <tanakamaruko1121n@gmail.com>
2023-02-25 11:09:37 +0900

    Delete old axios.post part

--- index.js
+++ index.js
@@ -5,26 +5,6 @@ const token = dotenv.config().parsed.lineToken;
 const url = dotenv.config().parsed.lineUrl;
 const message = dotenv.config().parsed.message;
 
-axios.post(
-    url, 
-    {
-        message,
-    }, 
-    {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer ' + token
-        }
-    }
-).then(function (response) {
-    console.log(response.data);
-    
-}).catch(function (error) {
-    console.log(error);
-    
-});;
-
-
 const handle = async function(){
     const pubMsg = await axios.post(
         url, 
